66d28648aedd39b58270188db2bf08338aaeda0e
puneetd <pd4500@protonmail.com>
2025-09-01 13:45:56 -0600

    weird rows

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import flet as ft
 from threading import Thread
 from PIL import UnidentifiedImageError
 from io import BytesIO
+import os
 
 class Rembg:
     def __init__(self, page: ft.Page):
@@ -16,7 +17,9 @@ class Rembg:
                 ft.NavigationBarDestination(icon=ft.Icons.HOME,
                                             label="Home"),
                 ft.NavigationBarDestination(icon=ft.Icons.SETTINGS,
-                                            label="Preferences")
+                                            label="Preferences"),
+                ft.NavigationBarDestination(icon=ft.Icons.QUESTION_MARK,
+                                            label="About")
             ]
         )
 
@@ -93,7 +96,14 @@ class Rembg:
         print(fpath)
         with open(fpath, 'rb') as f:
             ini = f.read()
-
+        platform = self.page.platform
+        mobile = ['PagePlatform.ANDROID', 'PagePlatform.IOS']
+        for lists in mobile:
+            if platform == lists:
+                ini = e.files[0].bytes
+            else:
+                with open(fpath, 'rb') as f:
+                    ini = f.read()
         self.modal = None
         try:
           self.outi = rembg.remove(ini, force_return_bytes=True, session=self.sess)
@@ -102,6 +112,26 @@ class Rembg:
                              "Python couldn't identify this as an image.", 
                              actions=[ft.TextButton("Ok", on_click=lambda e: self.page.close(self.modal))]
                             )
+
+        fname = os.path.basename(fpath)
+        splits = os.path.splitext(fname)
+        self.name = f"{splits[0]}(no-bg).png"
+
+        self.row_cons = [ft.TextField(label="Filename", 
+                                      value=self.name,
+                                      width=self.page.width - 200),
+                         ft.Row(controls=[ft.IconButton(icon=ft.Icons.COPY, 
+                                       icon_size=20,
+                                       on_click=self.copy,),
+                         ft.IconButton(icon=ft.Icons.SAVE, 
+                                       icon_size=20,
+                                       )], alignment=ft.MainAxisAlignment.END)]
+        self.img_cons = [ft.Row(controls=self.row_cons,
+                                wrap=True,
+                                spacing=5,)]
+        self.cons.controls.clear()
+        self.cons.controls.extend(self.img_cons)
+        self.page.update()
 
     def alertdialog(self, titles, text, actions):
         # this is a wrapper function for the dialog,
@@ -117,6 +147,22 @@ class Rembg:
         self.page.open(self.modal)
         self.page.update()
 
+    def copy(self, data):
+        self.page.set_clipboard(data)
+
+        self.banner = ft.Banner(
+            bgcolor=ft.Colors.BLUE,
+            content=ft.Text(
+                "Image saved to clipboard!"
+            ),
+            actions=[
+                ft.TextButton("Ok", on_click=close)
+            ]
+        )
+
+    def close(e):
+        self.banner.open = False
+        self.page.update()
 def main(page: ft.Page):
     Rembg(page)
     page.update()
