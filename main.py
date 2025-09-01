4db5fa324d97b029901c93096a3c3ceb0f848db4
puneetd <pd4500@protonmail.com>
2025-09-01 11:55:36 -0600

    add working rembg

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import flet as ft
+from threading import Thread
+from PIL import UnidentifiedImageError
+from io import BytesIO
 
 class Rembg:
     def __init__(self, page: ft.Page):
@@ -8,12 +11,29 @@ class Rembg:
         self.page.horizontal_alignment = ft.CrossAxisAlignment.STRETCH
         self.page.theme = ft.Theme(font_family="Cantarell")
 
-        # appbar (gtk headerbar)
-        self.appbar = ft.AppBar(
-            leading=ft.IconButton(icon=ft.Icons.MENU, tooltip="HWO", on_click=self.draw),
-            center_title=True,
+        self.page.navigation_bar = ft.NavigationBar(
+            destinations=[
+                ft.NavigationBarDestination(icon=ft.Icons.HOME,
+                                            label="Home"),
+                ft.NavigationBarDestination(icon=ft.Icons.SETTINGS,
+                                            label="Preferences")
+            ]
         )
 
+        # appbar (gtk headerbar)
+        self.appbar = ft.AppBar(
+            center_title=True,
+        )
+        
+        # this is for loading screen
+        self.loading_cons = [ft.Text('Loading rembg...', size=45, 
+                                   weight=ft.FontWeight.W_900,
+                                   text_align=ft.TextAlign.CENTER,
+                                   width=self.page.width
+                                   ),
+                             ft.Text('The AI model for removing backgrounds is loading, this will take 10-20 seconds.', 
+                                   text_align=ft.TextAlign.CENTER,
+                                   width=self.page.width),]
         # main items for column
         self.main_cons = [ft.Text('rembg-flutter', size=45, 
                                    weight=ft.FontWeight.W_900,
@@ -32,43 +52,70 @@ class Rembg:
         ## main content (column)
         self.cons = ft.Column(alignment=ft.alignment.center,
                               horizontal_alignment=ft.CrossAxisAlignment.CENTER
-                              ,controls=self.main_cons,
+                              ,controls=self.loading_cons,
                               )
 
         # main file picker for file
         self.file_picker = ft.FilePicker(on_result=self.image_file)
 
-        self.draws = ft.NavigationDrawer(
-            controls=[
-                ft.NavigationDrawerDestination(
-                    label="Home",
-                    icon=ft.Icons.HOME_FILLED
-                ),
-                ft.NavigationDrawerDestination(
-                    label="Prefernces",
-                    icon=ft.Icons.SETTINGS
-                ),
-                ft.NavigationDrawerDestination(
-                    label="About",
-                    icon=ft.Icons.QUESTION_MARK
-                ),
-            ]
-        )
         # pagelet (main body)
         self.pagelet = ft.Pagelet(
             appbar=self.appbar,
             content=self.cons,
-            drawer=self.draws
         )
 
-        self.page.add(self.pagelet, self.draws, self.file_picker)
-    def draw(self, e):
-        self.page.open(self.draws) 
+        self.page.add(self.pagelet, self.file_picker)
+        # this loads the rembg waiting screen
+        self.required_threading()
+    def required_threading(self):
+        # this is for not freezing the ui on the main thread
+        Thread(target=self.rembg_start, daemon=True).start()
+        print("Starting rembg thread...")
+    def rembg_start(self):
+        # import rembg and start a new session
+        import rembg
+        print("imported rembg")
+        self.sess = rembg.new_session()
+        # this takes the rembg function-level import and puts it globally
+        # not recommended, but this is the only (and easiest) way
+        globals()['rembg'] = rembg
+        print("rembg globalized")
+        # after that, we delete the loading widgets 
+        self.cons.controls.clear()
+        # then add the main ones
+        print("controls cleared")
+        self.cons.controls.extend(self.main_cons)
+        self.page.update()
 
     def image_file(self, e: ft.FilePickerResultEvent):
         if e.files:
-            for file in e.files:
-                print(file.path)
+           fpath = [file.path for file in e.files][0]
+        print(fpath)
+        with open(fpath, 'rb') as f:
+            ini = f.read()
+
+        self.modal = None
+        try:
+          self.outi = rembg.remove(ini, force_return_bytes=True, session=self.sess)
+        except UnidentifiedImageError:
+            self.alertdialog("Failure", 
+                             "Python couldn't identify this as an image.", 
+                             actions=[ft.TextButton("Ok", on_click=lambda e: self.page.close(self.modal))]
+                            )
+
+    def alertdialog(self, titles, text, actions):
+        # this is a wrapper function for the dialog,
+        # so it doesn't have to be repeated 
+        self.modal = ft.AlertDialog(
+            modal=False,
+            title=ft.Text(f'{titles}'),
+            content=ft.Text(f'{text}'),
+            actions=actions,
+            actions_alignment=ft.MainAxisAlignment.END,
+            on_dismiss=lambda e: print("Dismissed by user")
+        )
+        self.page.open(self.modal)
+        self.page.update()
 
 def main(page: ft.Page):
     Rembg(page)
