a6868eaf42b67edce81fc3d34da36408a1c9c753
puneetd <pd4500@protonmail.com>
2025-09-03 19:34:03 -0600

    small update

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from threading import Thread
 from PIL import UnidentifiedImageError
 from io import BytesIO
 import os
+import base64
+from functools import partial
 
 class Rembg:
     def __init__(self, page: ft.Page):
@@ -11,6 +13,7 @@ class Rembg:
         self.page.vertical_alignment = ft.MainAxisAlignment.CENTER
         self.page.horizontal_alignment = ft.CrossAxisAlignment.STRETCH
         self.page.theme = ft.Theme(font_family="Cantarell")
+        self.page.expand = True
 
         self.page.navigation_bar = ft.NavigationBar(
             destinations=[
@@ -31,21 +34,17 @@ class Rembg:
         # this is for loading screen
         self.loading_cons = [ft.Text('Loading rembg...', size=45, 
                                    weight=ft.FontWeight.W_900,
-                                   text_align=ft.TextAlign.CENTER,
-                                   width=self.page.width
+                                   text_align=ft.TextAlign.CENTER
                                    ),
                              ft.Text('The AI model for removing backgrounds is loading, this will take 10-20 seconds.', 
-                                   text_align=ft.TextAlign.CENTER,
-                                   width=self.page.width),]
+                                   text_align=ft.TextAlign.CENTER,)]
         # main items for column
         self.main_cons = [ft.Text('rembg-flutter', size=45, 
                                    weight=ft.FontWeight.W_900,
-                                   text_align=ft.TextAlign.CENTER,
-                                   width=self.page.width
+                                   text_align=ft.TextAlign.CENTER
                                    ),
                           ft.Text('Remove a background, now with flutter.', 
-                                   text_align=ft.TextAlign.CENTER,
-                                   width=self.page.width),
+                                   text_align=ft.TextAlign.CENTER,),
                           ft.FilledButton(text='Continue w/ File', 
                                           icon=ft.Icons.START,
                                           adaptive=True,
@@ -54,9 +53,16 @@ class Rembg:
 
         ## main content (column)
         self.cons = ft.Column(alignment=ft.alignment.center,
-                              horizontal_alignment=ft.CrossAxisAlignment.CENTER
-                              ,controls=self.loading_cons,
+                              horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+                              controls=self.loading_cons,
+                              expand=True,
+                              scroll='always'
                               )
+
+        self.coan = ft.Container(expand=True,
+                                 content=self.cons,
+                                 alignment=ft.alignment.center,
+                                 )
 
         # main file picker for file
         self.file_picker = ft.FilePicker(on_result=self.image_file)
@@ -64,7 +70,7 @@ class Rembg:
         # pagelet (main body)
         self.pagelet = ft.Pagelet(
             appbar=self.appbar,
-            content=self.cons,
+            content=self.coan,
         )
 
         self.page.add(self.pagelet, self.file_picker)
@@ -74,6 +80,8 @@ class Rembg:
         # this is for not freezing the ui on the main thread
         Thread(target=self.rembg_start, daemon=True).start()
         print("Starting rembg thread...")
+    def on_page_resize(self, e):
+        self.page.update()
     def rembg_start(self):
         # import rembg and start a new session
         import rembg
@@ -95,18 +103,18 @@ class Rembg:
            fpath = [file.path for file in e.files][0]
         print(fpath)
         with open(fpath, 'rb') as f:
-            ini = f.read()
+            self.ini = f.read()
         platform = self.page.platform
         mobile = ['PagePlatform.ANDROID', 'PagePlatform.IOS']
         for lists in mobile:
             if platform == lists:
-                ini = e.files[0].bytes
+                self.ini = e.files[0].bytes
             else:
                 with open(fpath, 'rb') as f:
-                    ini = f.read()
+                    self.ini = f.read()
         self.modal = None
         try:
-          self.outi = rembg.remove(ini, force_return_bytes=True, session=self.sess)
+            self.outi = rembg.remove(self.ini, force_return_bytes=True, session=self.sess)
         except UnidentifiedImageError:
             self.alertdialog("Failure", 
                              "Python couldn't identify this as an image.", 
@@ -117,22 +125,30 @@ class Rembg:
         splits = os.path.splitext(fname)
         self.name = f"{splits[0]}(no-bg).png"
 
-        self.row_cons = [ft.TextField(label="Filename", 
-                                      value=self.name,
-                                      width=self.page.width - 200),
-                         ft.Row(controls=[ft.IconButton(icon=ft.Icons.COPY, 
-                                       icon_size=20,
-                                       on_click=self.copy,),
-                         ft.IconButton(icon=ft.Icons.SAVE, 
-                                       icon_size=20,
-                                       )], alignment=ft.MainAxisAlignment.END)]
-        self.img_cons = [ft.Row(controls=self.row_cons,
-                                wrap=True,
-                                spacing=5,)]
+        buffer = BytesIO(self.outi)
+        self.b64_outi = base64.b64encode(buffer.getvalue()).decode()
+
+        self.img_cons = [ft.ListTile(title=ft.TextField(label="Filename", 
+                                     value=self.name,
+                                     ),
+                                     trailing=ft.PopupMenuButton(
+                                        icon=ft.Icons.MORE_VERT,
+                                        items=[
+                                            ft.PopupMenuItem(icon=ft.Icons.COPY,
+                                                             text="Copy",
+                                                             on_click=partial(self.copy)),
+                                            ft.PopupMenuItem(icon=ft.Icons.SAVE,
+                                                             text="Save to storage",
+                                                             on_click=partial(self.save))
+                                        ]
+                                     )),
+                         ft.Image(
+                            src_base64=self.b64_outi,
+                            fit=ft.ImageFit.CONTAIN,
+                         )]
         self.cons.controls.clear()
         self.cons.controls.extend(self.img_cons)
         self.page.update()
-
     def alertdialog(self, titles, text, actions):
         # this is a wrapper function for the dialog,
         # so it doesn't have to be repeated 
@@ -147,22 +163,32 @@ class Rembg:
         self.page.open(self.modal)
         self.page.update()
 
-    def copy(self, data):
-        self.page.set_clipboard(data)
-
-        self.banner = ft.Banner(
-            bgcolor=ft.Colors.BLUE,
-            content=ft.Text(
-                "Image saved to clipboard!"
-            ),
-            actions=[
-                ft.TextButton("Ok", on_click=close)
-            ]
+    def copy(self, e):
+        self.modal = None
+        self.alertdialog(
+            titles="Copied!",
+            text="Copied to clipboard as a base64 hash.",
+            actions=[ft.TextButton("Ok", on_click=lambda e: self.page.close(self.modal))]
         )
+        self.page.set_clipboard(self.b64_outi)
 
-    def close(e):
-        self.banner.open = False
-        self.page.update()
+    def save(self, e):
+        self.fpicker = ft.FilePicker(on_result=save_file)
+
+        self.page.add(self.fpicker)
+        self.fpicker.save_file(
+            dialog_title="Save as...",
+            file_name=self.name,
+            allowed_file_extensions=['png']
+        )
+        
+        def save_file(e: ft.FilePickerResultEvent):
+            if e.path:
+                with open(e.path, "wb") as f:
+                    f.write(self.ini)
+                print('file written')
+
+
 def main(page: ft.Page):
     Rembg(page)
     page.update()
