34f876b073c6481a9f29db3c924e6f43f6f4fbbd
puneetd <pd4500@protonmail.com>
2025-09-06 14:37:10 -0600

    good app, but cant build apk

--- main.py
+++ main.py
@@ -25,9 +25,22 @@ class Rembg:
                 ft.NavigationBarDestination(icon=ft.Icons.QUESTION_MARK,
                                             label="About")
             ],
-            on_change=self.on_nav_change
+            on_change=self.on_nav_change,
+            selected_index=0
         )
+        # this is for about page
+        self.version = '1.0.0'
 
+        if self.page.client_storage.contains_key('theme'):
+            theme = self.page.client_storage.get('theme')
+            self.theme_val = theme
+            if theme.upper() == 'DARK':
+                self.page.theme_mode = ft.ThemeMode.DARK
+            if theme.upper() == 'LIGHT':
+                self.page.theme_mode = ft.ThemeMode.LIGHT
+            self.page.update()
+        else:
+            self.theme_val = 'System'
         # appbar (gtk headerbar)
         self.appbar = ft.AppBar(
             center_title=True,
@@ -51,7 +64,91 @@ class Rembg:
                                           adaptive=True,
                                           on_click= lambda _: self.file_picker.pick_files()
                                           )]
+        self.credits_cons = "Created by stainlesteel.\n Licensed under the GNU General Public License v3, \nrefer to it for details on usage, distribution, and copying."
+        
+        self.about_cons = [
+                             ft.Text(
+                                 'rembg-flutter',
+                                 size=45,
+                                 weight=ft.FontWeight.W_900,
+                                 text_align=ft.TextAlign.CENTER
+                             ),
+                             ft.Text(
+                                 'a simple project to use the rembg library,\nby stainlesteel',
+                                 text_align=ft.TextAlign.CENTER,
+                                 size=25
+                             ),
+                             ft.FilledTonalButton(content=ft.Text(f'{self.version}')),
+                             ft.Card(
+                                 ft.Container(
+                                     content=ft.ListTile(
+                                         leading=ft.Icon(ft.Icons.WARNING),
+                                         title=ft.Text("Report an issue", size=20, weight=ft.FontWeight.W_500),
+                                         trailing=ft.Icon(ft.Icons.LINK),
+                                         url="https://example.com"
+                                     ),
+                                     margin=ft.margin.all(5)
+                                 )
+                             ),
+                             ft.Card(
+                                 ft.Container(
+                                     content=ft.Column([ft.ListTile(
+                                         leading=ft.Icon(ft.Icons.PEOPLE_OUTLINE),
+                                         title=ft.Text("Credits", size=20, weight=ft.FontWeight.W_500),
+                                         trailing=ft.IconButton(
+                                             icon=ft.Icons.ARROW_RIGHT,
+                                             on_click=self.credits
+                                         )
+                                        ),
+                                        ft.ListTile(
+                                              leading=ft.Icon(ft.Icons.PAGEVIEW),
+                                              title=ft.Text("Website", size=20, weight=ft.FontWeight.W_500),
+                                              trailing=ft.IconButton(icon=ft.Icons.LINK),
+                                              url="https://example.com"
+                                             )
+                                                        ]),
+                                     margin=ft.margin.all(5)
+                                    )
+                                 )
+        ]
+       #for the about page
 
+        # for dropdown theme list
+        self.themes = ['System', 'Light', 'Dark']
+        self.ops = []
+        for theme in self.themes:
+            self.ops.append(
+                ft.DropdownOption(
+                    key=theme,
+                    content=ft.Text(value=theme)
+                )
+            )
+        # for the settings page
+        self.settings_cons = [
+             ft.Text(
+                 'Preferences',
+                 size=45,
+                 weight=ft.FontWeight.W_900,
+                 text_align=ft.TextAlign.CENTER
+             ),
+             ft.Card(
+                 ft.Container(
+                     content=ft.Column([ft.ListTile(
+                         leading=ft.Icon(ft.Icons.PEOPLE_OUTLINE),
+                         title=ft.Text("Colour Scheme", size=20, weight=ft.FontWeight.W_500),
+                         trailing=ft.Dropdown(
+                            label=self.theme_val,
+                            options=self.ops,
+                            on_change=self.set_theme
+                        )
+                        ),
+                                        ]),
+                     margin=ft.margin.all(5)
+                    )
+                 )
+        ]
+
+        self.modal = None
         ## main content (column)
         self.cons = ft.Column(alignment=ft.alignment.center,
                               horizontal_alignment=ft.CrossAxisAlignment.CENTER,
@@ -73,9 +170,7 @@ class Rembg:
             appbar=self.appbar,
             content=self.coan,
         )
-
-        self.about_cons = []
-
+        self.index = None
         self.page.add(self.pagelet, self.file_picker)
         # this loads the rembg waiting screen
         self.required_threading()
@@ -86,30 +181,67 @@ class Rembg:
     def on_page_resize(self, e):
         # this is for the container
         self.page.update()
+    def set_theme(self, e):
+        theme = e.control.value
+        self.page.client_storage.set('theme', theme)
+
+        if theme.upper() == 'DARK':
+            self.page.theme_mode = ft.ThemeMode.DARK
+        if theme.upper() == 'LIGHT':
+            self.page.theme_mode = ft.ThemeMode.LIGHT
+        self.page.update()
     def on_nav_change(self, e):
         self.cons.controls.clear()
+        self.index = e.control.selected_index
+        print(f'index: {self.index}')
+        if self.index == 0:
+            if not self.sess:
+                self.cons.controls.extend(self.loading_cons)
+            if hasattr(self, 'img_cons'):
+                self.appbar.leading = ft.IconButton(
+                        icon=ft.Icons.ARROW_BACK,
+                        tooltip="Go Back to Menu",
+                        on_click=partial(self.back)
+                    )
+                self.cons.controls.extend(self.img_cons)
+            else:
+                self.appbar.leading = None
+                self.cons.controls.extend(self.main_cons)
+        elif self.index == 1:
+            self.appbar.leading = None
+            self.cons.controls.extend(self.settings_cons)
+        elif self.index == 2:
+            self.appbar.leading = None
+            self.cons.controls.extend(self.about_cons)
+        self.page.update()
+    def credits(self, e):
+        self.alertdialog('Credits',
+                         f'{self.credits_cons}',
+                         actions=[ft.TextButton("Ok", on_click=lambda e: self.page.close(self.modal))])
     def rembg_start(self):
         # import rembg and start a new session
         import rembg
         print("imported rembg")
-        self.sess = rembg.new_session()
+        u2_path = os.path.join(os.path.dirname(__file__), "assets", "u2net.onnx")
+        self.sess = rembg.new_session("u2net_custom", model_path=u2_path)
         # this takes the rembg function-level import and puts it globally
         # not recommended, but this is the only (and easiest) way
         globals()['rembg'] = rembg
         print("rembg globalized")
         # after that, we delete the loading widgets 
-        self.cons.controls.clear()
         # then add the main ones
-        print("controls cleared")
-        self.cons.controls.extend(self.main_cons)
+
+        # this is for not switching userview if they aren't in the main page
+        if self.index == None:
+            self.cons.controls.clear()
+            print("controls cleared")
+            self.cons.controls.extend(self.main_cons)
         self.page.update()
 
     def image_file(self, e: ft.FilePickerResultEvent):
         if e.files:
            fpath = [file.path for file in e.files][0]
         print(fpath)
-        with open(fpath, 'rb') as f:
-            self.ini = f.read()
         platform = self.page.platform
         mobile = ['PagePlatform.ANDROID', 'PagePlatform.IOS']
         for lists in mobile:
@@ -136,7 +268,6 @@ class Rembg:
         self.extra()
 
     def extra(self):
-
         # look at that indentation!
         self.img_cons = [ft.ListTile(title=ft.TextField(label="Filename", 
                                      value=self.name,
@@ -170,6 +301,7 @@ class Rembg:
         self.cons.controls.extend(self.main_cons)
         self.appbar.leading = None
         self.page.update()
+
     def alertdialog(self, titles, text, actions):
         # this is a wrapper function for the dialog,
         # so it doesn't have to be repeated 
@@ -218,9 +350,8 @@ class Rembg:
                 file_name=self.name,
         )
 
-
 def main(page: ft.Page):
     Rembg(page)
     page.update()
 
-ft.app(main)
+ft.app(target=main, assets_dir="assets")
